fc58a893a30817468792eb2eea928e8d77c4f0aa
Narges <se_es1471@yahoo.com>
2020-06-06 08:28:23 +0430

    add new project

---
+++ main.js
@@ -0,0 +1,11 @@
+// window.addEventListener('DomContentLoaded',function (e) {
+//     document.querySelector('presentation').onclick=HandlePresnetationClick;
+// });
+// HandlePresnetationClick(e){
+//     alert('clicked');
+// }
+
+$(document).ready(function() {
+    $('#click').click(function(){
+    });
+});
